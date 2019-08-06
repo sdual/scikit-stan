061034836a1e6fc8fd6420df24093234944fa1f9
sdual <shr.nrht.0623@gmail.com>
2019-08-06 12:16:31 +0900

    wip

--- tests/backend/stan/model/linear_regression_test.py
+++ tests/backend/stan/model/linear_regression_test.py
@@ -6,14 +6,40 @@ from skstan.params import StanLinearRegressionParams
 
 
 class TestStanLinearRegression:
-
-    def test_fit(self):
-        # Test that a model is trained by using `fit` method.
+    def test_fit(self, monkeypatch):
+        # Test that a stan model is trained by using `fit` method.
         params = StanLinearRegressionParams(
-            chains=3, warmup=1000, n_itr=5000, n_jobs=1, algorithm='NUTS',
-            verbose=False, shrinkage=10,
+            chains=3,
+            warmup=1000,
+            n_itr=5000,
+            n_jobs=1,
+            algorithm='NUTS',
+            verbose=False,
+            shrinkage=10,
         )
+
+        def mock_laod_model(dummy):
+            return None
+
+        monkeypatch.setattr(StanLinearRegresson, 'load_model', mock_load_model)
+
         slr = StanLinearRegression(params)
+        X = np.array([[5.1, 3.5, 1.4, 0.2], [5.4, 3.4, 1.7, 0.2],
+                      [7., 3.2, 4.7, 1.4], [5., 2., 3.5, 1.],
+                      [5.9, 3.2, 4.8, 1.8], [6.3, 3.3, 6., 2.5],
+                      [6.9, 3.2, 5.7, 2.3]])
+
+        y = np.array([
+            0,
+            0,
+            1,
+            1,
+            1,
+            2,
+            2,
+        ])
+
+        slr.fit(X, y)
 
     def test_get_model_file_name(self, monkeypatch):
         # Test that model file name is returned.
@@ -21,11 +47,15 @@ class TestStanLinearRegression:
         def mock_load_model(dummy):
             return None
 
-        monkeypatch.setattr(StanModelLoadMixin,
-                            'load_model', mock_load_model)
+        monkeypatch.setattr(StanModelLoadMixin, 'load_model', mock_load_model)
         params = StanLinearRegressionParams(
-            chains=3, warmup=1000, n_itr=5000, n_jobs=1, algorithm='NUTS',
-            verbose=False, shrinkage=10,
+            chains=3,
+            warmup=1000,
+            n_itr=5000,
+            n_jobs=1,
+            algorithm='NUTS',
+            verbose=False,
+            shrinkage=10,
         )
         slr = StanLinearRegression(params)
         actual = slr.get_model_file_name()
@@ -34,32 +64,34 @@ class TestStanLinearRegression:
 
 
 class TestStanLogisticRegression:
-
     def test_get_model_file_name(self, monkeypatch):
         # Test that model file name is returned.
         def mock_load_model(dummy):
             return None
 
-        monkeypatch.setattr(StanModelLoadMixin,
-                            'load_model', mock_load_model)
-        slr = StanLogisticRegression(chains=3, warmup=1000, shrinkage=10,
-                                     n_jobs=1, n_itr=5000)
+        monkeypatch.setattr(StanModelLoadMixin, 'load_model', mock_load_model)
+        slr = StanLogisticRegression(chains=3,
+                                     warmup=1000,
+                                     shrinkage=10,
+                                     n_jobs=1,
+                                     n_itr=5000)
         actual = slr.get_model_file_name()
         expected = 'logistic_regression.pkl'
         assert actual == expected
 
 
 class TestStanPoissionRegression:
-
     def test_get_model_file_name(self, monkeypatch):
         # Test that model file name is returned.
         def mock_load_model(dummy):
             return None
 
-        monkeypatch.setattr(StanModelLoadMixin,
-                            'load_model', mock_load_model)
-        spr = StanPoissonRegression(chains=3, warmup=1000, shrinkage=10,
-                                    n_jobs=1, n_itr=5000)
+        monkeypatch.setattr(StanModelLoadMixin, 'load_model', mock_load_model)
+        spr = StanPoissonRegression(chains=3,
+                                    warmup=1000,
+                                    shrinkage=10,
+                                    n_jobs=1,
+                                    n_itr=5000)
         actual = spr.get_model_file_name()
         expected = 'poisson_regression.pkl'
         assert actual == expected
